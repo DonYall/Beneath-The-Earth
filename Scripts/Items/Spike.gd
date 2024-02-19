5c3ccde9950f9d8d6e843af3afb83e9db68fe25d
Sayna <nwott1@gmail.com>
2024-02-19 09:43:01 -0500

    Finished spike item

---
+++ Scripts/Items/Spike.gd
@@ -0,0 +1,15 @@
+extends "res://Scripts/Items/Item.gd"
+
+@export var damage = 2 # amount of damage to deal to the player
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func consume():
+	# TODO: deal damage to player
+	pass
+
+func on_enemy_enter():
+	# TODO: kill enemy
+	pass
