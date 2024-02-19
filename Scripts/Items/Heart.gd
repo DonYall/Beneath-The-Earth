6d778470e95114aa05bdf9a40bfbf94c17c9ddc2
Sayna <nwott1@gmail.com>
2024-02-19 09:35:10 -0500

    Finished health item

--- Scripts/Items/Heart.gd
+++ Scripts/Items/Heart.gd
@@ -1,8 +1,12 @@
 extends "res://Scripts/Items/Item.gd"
 
+@export var health_increment = 1 # how much health to add to the player on pick up
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-func on_pickup():
-	print("heart")
+func consume():
+	# TODO: add to player health
+	print("Picked up health, add to player's health")
+	pass
--- Scripts/Items/Item.gd
+++ Scripts/Items/Item.gd
@@ -8,5 +8,15 @@ func _ready():
 func _process(delta):
 	pass
 
+# when the item gets picked up
+# mock abstract function, meaning all items need to implement it
 func on_pickup():
-	print("base class")
+	consume()
+
+	# destroy the item when pick up
+	queue_free()
+
+# when the item gets used
+# mock abstract function, meaning all items need to implement it
+func consume():
+	push_error("UNIMPLEMENTED ERROR: consume()")
