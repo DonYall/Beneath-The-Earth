79c23b841795dac27ffc85da19982827593d44c1
Sayna <nwott1@gmail.com>
2024-02-23 10:36:01 -0500

    CompressedTexture2D array

---
+++ Scripts/Items/Fossil.gd
@@ -0,0 +1,7 @@
+extends "res://Scripts/Items/Item.gd"
+
+@export var fossil_textures: Array[CompressedTexture2D] = []
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
