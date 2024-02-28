e3c26cc2e57961365bc2c55c41fa4f1d90fc3746
Sayna <nwott1@gmail.com>
2024-02-28 17:33:04 -0500

    Adjusted enemy size

--- Scenes/Enemy.gd
+++ Scenes/Enemy.gd
@@ -30,5 +30,5 @@ func _process(delta):
 
 		if velocity.length() > 0:
 			velocity = velocity.normalized() * speed
-		position += velocity * delta
+		move_and_collide(velocity * delta)
 		position = position.clamp(Vector2.ZERO, screen_size)
