43a5d91e4d21e6af84bd3d79a7c33e4f5a8f365e
usomarriba <usomarriba@kx.com>
2024-02-27 22:00:09 -0500

    Add cells that break when player enters them.

---
+++ Scenes/Grid/grid_cell.gd
@@ -0,0 +1,22 @@
+extends Area2D
+
+signal cell_ready_to_be_destroyed
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+# Once player enters the grid cell, the cells breaks and sends out a signal
+# saying it is ready to be destroyed.
+# TODO - determine whether or not to break cell here or have a controller handle this.
+func _on_area_entered(body):
+	if body.get_name() == "Player":
+		# print("Cell ready to break!")
+		emit_signal("cell_ready_to_be_destroyed")
+		queue_free()
